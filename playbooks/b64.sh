d08a0d875a478772bae6bd1fc0b054f6efe165d9
3452818 <mit3452818@gmail.com>
2025-08-08 11:50:54 +0000

    add ansible

---
+++ playbooks/b64.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+base64 /home/user1/.ssh/id_rsa.pub > /home/user1/.ssh/id_rsa.pub.b64
---
+++ playbooks/decode.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+base64 -d /home/user1/.ssh/id_rsa.pub.b64 > /home/user1/.ssh/id_rsa.pub4
+
